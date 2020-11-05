3a5fc4a7bbe7568691e59041c08a18066323efca
keval718 <kevalvirat@gmail.com>
2020-11-05 02:36:03 -0500

    first

---
+++ src/components/projects.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from 'react'
+
+export default class Projects extends Component {
+  render() {
+    return (
+      <div>
+				<section className="colorlib-work" data-section="projects">
+					<div className="colorlib-narrow-content">
+						<div className="row">
+							<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+								<span className="heading-meta">My Work</span>
+								<h2 className="colorlib-heading animate-box">Recent Projects</h2>
+							</div>
+						</div>
+						<article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2> Seo Access</h2> <a href="https://seoaccess.herokuapp.com/" > Click Here</a>
+
+                       <ul>
+					 <li> 	Developed a Responsive web Scraper which scrap all the HTML/CSS from a website to generate an AODA(Accessibility) WCAG 2.0 or 2.1 AA and SEO analysis report.</li>
+					 <li>	Creating a NodeJS based framework for scraping and validating results based on website URL and recording that result into database using MongoDB and depicting results as an interactive report on the webpage.</li>
+					</ul>
+
+                      </div>
+                    </div>
+                  </article>
+				  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2> Grocers </h2> <a href="https://grocer-user.herokuapp.com/" > Click Here</a><br/>
+
+						•	Developed a Responsive web application that allows user to do grocery shopping online and just visit the store for pickup and user can also select nearby grocery store using our online maps.<br/>
+						•	It reduces about 50% of users time by eliminating the wait time in long queue and roaming around the store to collect groceries.<br/>
+						•	Technologies used : Node, React, JavaScript, jQuery, MongoDB.
+
+
+                      </div>
+                    </div>
+                  </article>
+				  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2> Wish My Trip </h2> <a href="https://github.com/prashil117/WMT_User" > Click Here</a>
+
+						•	Developed a website that allows user to book a car with the best deals available. The website integrates information of different car providing agents and various prices and car models available<br/>
+						•	Website saves user time to call each different dealer for car availability and prices.<br/>
+						•	Reduce 30% time of car provider to generate hand written bill as this website generate bills also<br/>
+						•	Technologies used :- Node.js, Angular JS,  Typescript , MySQL, Laravel, PHP<br/>
+
+                      </div>
+                    </div>
+                  </article>
+				  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2> Let’s Shop  </h2> <a href="https://github.com/keval718/Lets-Shop" > Click Here</a>
+
+						•	Developed a full stack e-commerce web app suitable for a wide range of products or service based business <br/>
+						•	Created user interface with Semantic UI react, User authentication with JWT  and Cookies, Password hashing with bcrypt<br/>
+						•	Process payments with Stripe API, Dynamically upload image with Cloudinary API, Modelling database concept with Mongoose Schemas<br/>
+						•	Querying/Pagination/Mutating with MongoDB<br/>
+						•	Deployed app with serverless deployment service Now<br/>
+						•	Technologies used: - React, React Hooks, Google Cloud, Node.js, Express, Next.js, MongoDB, Semantic UI<br/>
+
+                      </div>
+                    </div>
+                  </article>
+							
+						
+							
+						
+						
+						<div className="row">
+							
+						</div>
+					</div>
+				</section>
+      </div>
+    )
+  }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,117 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Mosaic North America, Web Developer <span>Aug 2020-present</span></h2>
+                        •	Meet with the clients and team leads to identify the needs and design of the website.<br/>
+                        •	Create and test applications for a website.<br/>
+                        •	Write code for a website.<br/>
+                        •	Work with UX/UI designer to execute the design plan.<br/>
+                        •	Work with the team members to identify and add information and content onto the site including graphics, audio, and video. <br/>
+                        •	Edit and fix company websites to combat bugs and small errors.<br/>
+                        •	Manage projects by collaborating and communicating with supervisor and other designers and writers. <br/>
+                        •	Technologies used :- PHP, Nodejs, Laravel, WordPress, Shopify, Wix <br/>
+
+                      </div>
+                    </div>
+                  </article>
+
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Dare and Defy Strategic Consulting, Toronto, ON , Web Developer <span>Jan 2020-May 2020</span></h2>
+                        •	Design and developed a website using Node.js, React and MongoDB for database<br/>
+                        •	Meeting with the development team to discuss user interface ideas and applications.<br/>
+                        •	Reviewing application requirements and interface designs.<br/>
+                        •	Developing and implementing highly-responsive user interface components using React concepts like semantic UI, React Bootstrap<br/>
+                        •	Contributed in building application logic, Created Rest API for backend. Evaluate code to ensure that it is valid, is properly structured, meets company standards and is compatible with browsers and devices<br/>
+                        •	Identify problems by testing and correct problems.<br/>
+                        •	Develop database that support web applications. Perform web site tests and deployed it.<br/>
+
+
+                      </div>
+                    </div>
+                  </article>
+                 
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Hybriona Technologies , Web Developer <span>Feb 2018-Jan 2020</span></h2>
+                        •	Developed web site using React, Bootstrap CSS, HTML5, Semantic UI, Created API  using Express JS and Nodejs for backend <br/>
+                        •	Build application backend using Node JS, developed databases in MongoDB that support web application<br/>
+                        •	Develop responsive interface components using React concepts like React Bootstrap, Material Kit React<br/>
+                        •	Documenting application changes and developing updates.<br/>
+                        •	Reduce Time over 50% to generate bills by making a billing system in Angular.<br/>
+                        •	Work with talented, knowledge-driven individuals and share ideas to improve the product with the team.<br/>
+                        •	Work with the designers ,UI Team and QA Team to resolve bugs and also create new features<br/>
+
+
+
+                      </div>
+                    </div>
+                  </article>
+                         
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Humber College <span>2018-2020</span></h2>
+                        <p>I persuade my post graduation degree in Information and Technology Solutions. I have taken courses like Web Development, Object Oriented Programming, SQL Server database Development,
+                          Web Programming and Designing, C#, .NET, Big Data, React, NodeJS
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Ks School of business management  <span>2015-2018</span></h2>
+                        <p>I have completed my Bachelor’s in Computer Application and Information Technology. I have taken courses like - C, C++, Advance Java  , Data Structures and Algorithms, C#, .NET web development, Data Science</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
